b0b849fc8b5b10d9d3c1f90c408bca944ec4e31d
Suman Gupta <talk2suman@gmail.com>
2018-02-13 06:27:31 -0800

    Function exercises

---
+++ src/main/java/kpower/function/EmployeeFunction.java
@@ -0,0 +1,19 @@
+package kpower.function;
+
+import java.util.function.Function;
+
+public class EmployeeFunction {
+    public static void main(String[] args) {
+        Function<Integer,String> empExperienceFunction = (s)->{
+            if (s < 3) {
+                return "Employee with " + s + " years of experience is at Fresher level.";
+            }else{
+                return "Employee with " + s + " years of experience is at Senior level.";
+            }
+        };
+
+        System.out.println(empExperienceFunction.apply(1));
+        System.out.println(empExperienceFunction.apply(5));
+    }
+
+}
